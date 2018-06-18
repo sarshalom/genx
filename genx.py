18182f1b50f9446d81cb6c66e06c309f3ce8e4cf
hresheff <hezi_resheff@intuit.com>
2018-06-18 13:42:39 +0300

    - sparse_categorical_crossentropy  - added BEGIN/END tokens

--- genx.py
+++ genx.py
@@ -9,6 +9,19 @@ from keras.preprocessing.sequence import pad_sequences
 from sklearn.preprocessing import LabelEncoder
 
 
+# enum for special tokens
+class Tokens(object):
+    PADDING = 0
+    BEGIN = 1
+    END = 2
+
+
+# this will move to a settings file ...
+class Settings(object):
+    DICT_SIZE = 5000
+    MAX_SEQUENCE = 15
+
+
 def build_genx_model(n_users, n_items, n_words, embedding_size, word_embedding_size, rev_length, lstm_size):
 
     # item input
@@ -33,24 +46,25 @@ def build_genx_model(n_users, n_items, n_words, embedding_size, word_embedding_s
     lstm_init = keras.layers.Dense(lstm_size, activation='relu')(lstm_init)                    # so init size is correct
 
     lstm_generator = keras.layers.LSTM(lstm_size, return_sequences=True)(text_embedding, initial_state=[lstm_init, lstm_init])
-    text_output = keras.layers.TimeDistributed(keras.layers.Dense(n_words+2, activation='softmax'))(lstm_generator)
+    text_output = keras.layers.TimeDistributed(keras.layers.Dense(n_words+2, activation='softmax'), name="text-output")(lstm_generator)
 
     model = keras.Model(inputs=[user_input, item_input, text_input], outputs=[score, text_output])
     model.compile(keras.optimizers.Adam(1e-2),
-                  ['mean_squared_error', 'categorical_crossentropy'],
+                  ['mean_squared_error', 'sparse_categorical_crossentropy'],
                   loss_weights=[1, .1])
     model.summary()
     return model
 
 
-def get_data(DATA_FILE, n_words_max=1000, max_sequence_len=20):
+# TODO: make this a class with persistency for token maps etc.
+def get_data(DATA_FILE):
     dataset = pd.read_csv(DATA_FILE, header=None, sep='\t', nrows=None)
     dataset.columns = ['user_id', 'item_id', 'rating', 'review']
 
     print(dataset.head())
 
     # Tokenize
-    tok = Tokenizer(num_words=n_words_max,
+    tok = Tokenizer(num_words=Settings.DICT_SIZE,
                     filters='',
                     lower=True,
                     split=" ",
@@ -59,8 +73,9 @@ def get_data(DATA_FILE, n_words_max=1000, max_sequence_len=20):
 
     reviews = tok.texts_to_sequences(dataset.review.values)
 
-    # Pad sequences (right)
-    reviews = pad_sequences(reviews, maxlen=max_sequence_len, padding='post')
+    # Pad sequences (right) and add begin/end token. Regular tokens are shiften by 2 and now start from 3
+    reviews = [[Tokens.BEGIN] + list(np.array(rev, dtype=int) + 2) + [Tokens.END] for rev in reviews]
+    reviews = pad_sequences(reviews, maxlen=Settings.MAX_SEQUENCE, padding='post')
 
     # users, items
     users = LabelEncoder().fit_transform(dataset.user_id)
@@ -75,16 +90,17 @@ if __name__ == "__main__":
 
     model = build_genx_model(n_users=users.max()+1,
                              n_items=items.max()+1,
-                             n_words=1000,
+                             n_words=Settings.DICT_SIZE,
                              embedding_size=20,
                              word_embedding_size=20,
-                             rev_length=20,
+                             rev_length=Settings.MAX_SEQUENCE-1,
                              lstm_size=10)
 
-    rev_out = np.array([np.eye(1002)[rev] for rev in reviews])
-    model.fit([users, items, reviews], [ratings, rev_out], epochs=1)
+    # Input doesn't need END token, output doesn't need BEGIN token
+    rev_in = reviews[:, :-1]
+    rev_out = reviews[:, 1:].reshape(reviews.shape[0], reviews.shape[1]-1, 1)
 
-    # TODO: change model output method to something that will work with real data
-    # TODO: shift target word sequenes by 1 ..
+    model.fit([users, items, rev_in], [ratings, rev_out], epochs=1, verbose=2)
+
 
 
